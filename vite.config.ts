0ee39445bb9af8c74f48949c17c205e49afc9eb4
Inflyser <89538138944s@gmail.com>
2025-08-22 19:50:25 +0300

    package update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src') // Алиас для импортов
-    }
-  },
-  server: {
-    port: 3000,
-    host: true,
-    open: true // Автоматически открывать браузер
+  build: {
+    outDir: 'dist',                // Важно! Netlify ищет папку dist
+    sourcemap: false
   }
 })
