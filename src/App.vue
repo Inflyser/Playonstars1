72de74dd1ea7735055080ee37fefa78e89c07daf
Inflyser <89538138944s@gmail.com>
2025-08-27 01:00:40 +0300

    Back API

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
- 
-  <RouterView />
-
+  <AppLayout>
+    <TelegramInit />
+    <RouterView />
+  </AppLayout>
 </template>
 
 <script setup lang="ts">
-
+import AppLayout from '@/components/layout/AppLayout.vue';
+import TelegramInit from '@/components/layout/TelegramInit.vue';
 </script>
