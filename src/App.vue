56ee139ecc5662143d1e583d77290c2a53382bae
Inflyser <89538138944s@gmail.com>
2025-09-10 03:01:52 +0300

    TON

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@
 
 <script lang="ts">
 import TelegramInit from '@/components/layout/TelegramInit.vue';
+import { transactionMonitor } from '@/services/transactionMonitor';
+
+
+transactionMonitor.startMonitoring();
+transactionMonitor.onNewTransactions((txs) => {
+  console.log('New transactions:', txs);
+});
 
 export default {
   components: {
